b800cb2f581d1d330f0ba5e8ebce612409429930
2510subham <subhamagarwal400@gmail.com>
2023-12-15 18:50:46 +0530

    add some info

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -12,16 +12,16 @@ export const validateUser = async (req, res, next) => {
         next();
     } catch (error) {
         return res.status(401).send({
-                success: false,
-                message: "Error in verifying token",
-            });
+            success: false,
+            message: "Error in verifying token",
+        });
     }
 };
 
 // admin access
 export const isAdmin = async (req, res, next) => {
     try {
-        const user = await auth.findById(req.user._id);
+        const user = await auth.findById(req.user._id);//check for the role with the id of the user which is stored in the token
         if (user.role !== 'admin') {
             return res.status(401).send({
                 success: false,
